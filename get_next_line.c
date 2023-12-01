06e4be40f1d9ddf63b502e0f876ef3ca0fa0bdee
mzhukova <mzhukova@student.42Wolfsburg.de>
2023-12-01 17:19:51 +0100

    start getnextline

---
+++ get_next_line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 16:44:09 by mzhukova          #+#    #+#             */
+/*   Updated: 2023/12/01 17:10:08 by mzhukova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd);
+
+int main (int argc, char **argv)
+{
+	open("")
+	//Get the argument
+	//Read the file, displaying each line, followd by a new line
+	
+
+
+}
---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 16:44:20 by mzhukova          #+#    #+#             */
+/*   Updated: 2023/12/01 16:58:59 by mzhukova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
