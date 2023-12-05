cdf0ddcb78cdc1d1f081c83672f57138e0fe9901
mzhukova <mzhukova@student.42Wolfsburg.de>
2023-12-05 13:36:13 +0100

    nothing works

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,42 @@
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 16:44:09 by mzhukova          #+#    #+#             */
-/*   Updated: 2023/12/04 12:04:54 by mzhukova         ###   ########.fr       */
+/*   Updated: 2023/12/05 13:33:05 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "get_next_line.h"
+#define BUFFER_SIZE 1024  // Size of the buffer
 
-char	*get_next_line(int fd);
-
-int main (int argc, char **argv)
+char	*get_next_line(int fd)
 {
-	char	buf[100];
-	int		nb_read;
+	char	*buffer;
+	static char *p;
+	static char *temp;
+	ssize_t	bytes_read;
 
-	
+	buffer = malloc(BUFFER_SIZE);
+	if (buffer == NULL)
+		return (NULL);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	p = temp;
+	while (*buffer != '\n' || *buffer != '\0')
+	{
+		temp = buffer;
+		temp++;
+		buffer++;
+	}
+	temp++;
+	free(buffer);
+	return (p);
+}
 
-	//Get the argument
-	//Read the file, displaying each line, followd by a new line
-	
+#include <stdio.h>
 
-
+int main ()
+{
+	char *res = get_next_line(0);
+	printf("Result: %s \n", res);
+	return (0);
 }
