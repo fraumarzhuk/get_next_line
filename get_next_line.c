88f92443cb10ecc41dfa94a58c8b1dd7e1f1642e
mzhukova <mzhukova@student.42Wolfsburg.de>
2023-12-11 15:51:48 +0100

    no leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 02:11:08 by mariannazhu       #+#    #+#             */
-/*   Updated: 2023/12/11 14:44:42 by mzhukova         ###   ########.fr       */
+/*   Updated: 2023/12/11 15:48:44 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,98 +16,100 @@
 # define BUFFER_SIZE 42
 #endif
 
-char	*get_start(const char *remainer)
+#include <stdio.h>
+
+char	*get_start(const char *remainder)
 {
 	int		len;
 	char	*start;
 	char	*temp;
 
 	len = 0;
-	while (remainer[len] != '\n' && remainer[len] != '\0')
+	while (remainder[len] != '\n' && remainder[len] != '\0')
 		len++;
 	start = malloc(len + 2);
 	if (!start)
 		return (NULL);
 	temp = start;
-	while (*remainer != '\n' && *remainer != '\0')
+	while (*remainder != '\n' && *remainder != '\0')
 	{
-		*temp = *remainer;
+		*temp = *remainder;
 		temp++;
-		remainer++;
+		remainder++;
 	}
-	if (*remainer == '\n')
+	if (*remainder == '\n')
 		*temp++ = '\n';
 	*temp = '\0';
 	return (start);
 }
 
-char	*get_remainer(const char *remainer)
+char	*get_remainder(const char *remainder)
 {
 	char	*end;
 	char	*temp;
 	int		n;
 
 	n = 0;
-	while (remainer[n] != '\n' && remainer[n] != '\0')
+	while (remainder[n] != '\n' && remainder[n] != '\0')
 		n++;
-	if (remainer[n] == '\0' || remainer[n + 1] == '\0' )
+	if (remainder[n] == '\0' || remainder[n + 1] == '\0' )
 		return (NULL);
-	end = malloc(ft_strlen(remainer) - n + 1);
+	end = malloc(ft_strlen(remainder) - n + 1);
 	if (!end)
 		return (NULL);
 	temp = end;
-	remainer += n + 1;
-	while (*remainer != '\0')
-		*temp++ = *remainer++;
+	remainder += n + 1;
+	while (*remainder != '\0')
+		*temp++ = *remainder++;
 	*temp = '\0';
 	return (end);
 }
 
-char	*read_and_store(char *buffer, char **remainer)
+char	*read_and_store(char *buffer, char **remainder)
 {
 	char	*res;
 	char	*temp;
-	char	*new_remainer;
+	char	*new_remainder;
 
 	res = NULL;
-	if (*remainer == NULL)
-		*remainer = ft_strdup(buffer);
+	if (*remainder == NULL)
+		*remainder = ft_strdup(buffer);
 	else
 	{
-		temp = ft_strjoin(*remainer, buffer);
-		free(*remainer);
-		*remainer = temp;
+		temp = ft_strjoin(*remainder, buffer);
+		free(*remainder);
+		*remainder = temp;
 	}
-	if (!*remainer)
+	if (!*remainder)
 		return (NULL);
-	if (ft_strrchr(*remainer, '\n') == NULL)
+	if (ft_strrchr(*remainder, '\n') == NULL)
 		return (NULL);
 	else
 	{
-		res = get_start(*remainer);
-		new_remainer = get_remainer(*remainer);
-		free(*remainer);
-		*remainer = new_remainer;
+		res = get_start(*remainder);
+		new_remainder = get_remainder(*remainder);
+		free(*remainder);
+		*remainder = new_remainder;
 		return (res);
 	}
 }
 
-char	*process_remainer(char **remainer)
+char	*process_remainder(char **remainder)
 {
 	char	*res;
 
 	res = NULL;
-	if (*remainer != NULL && **remainer != '\0')
+	if (*remainder != NULL && **remainder != '\0')
 	{
-		if (ft_strrchr(*remainer, '\n') != NULL)
+		if (ft_strrchr(*remainder, '\n') != NULL)
 		{
-			res = read_and_store("", remainer);
+			res = read_and_store("", remainder);
 		}
 		else
 		{
-			res = ft_strdup(*remainer);
-			free(*remainer);
-			*remainer = NULL;
+			res = ft_strdup(*remainder);
+			free(*remainder);
+			*remainder = NULL;
 		}
 	}
 	return (res);
@@ -115,47 +117,56 @@ char	*process_remainer(char **remainer)
 
 char	*get_next_line(int fd)
 {
-	static char	*remainer;
+	static char	*remainder;
 	char		buffer[BUFFER_SIZE + 1];
 	char		*res;
 	size_t		bytes_read;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-	{
-		remainer = NULL;
-		return (NULL);
-	}
+		return (free(remainder), remainder = NULL, NULL);
 	while (1)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read <= 0)
 			break ;
 		buffer[bytes_read] = '\0';
-		res = read_and_store(buffer, &remainer);
+		res = read_and_store(buffer, &remainder);
 		if (res != NULL)
 			return (res);
 	}
-	return (process_remainer(&remainer));
+	return (process_remainder(&remainder));
 }
 
-// #include <stdio.h>
+/*
+#include <stdio.h>
 
-// int main()
-// {
-// 	char	*res;
-// 	int	fd = open("baal.txt", O_RDONLY);
+int main()
+{
+	char	*res;
+	int	fd = open("baal.txt", O_RDONLY);
 
-// 	if (fd < 0)
-// 		printf("File not open \n");
+	if (fd < 0)
+		printf("File not open \n");
 
-// 	res = get_next_line(fd);
-// 	printf("Res1: %s", res);
-// 	res = get_next_line(fd);
-// 	printf("Res2: %s", res);
-// 	res = get_next_line(fd);
-// 	printf("Res3: %s", res);
-// 	res = get_next_line(fd);
-// 	printf("Res4: %s", res);
-// 	res = get_next_line(fd);
-// 	printf("Res4: %s", res);
-// }
+	res = get_next_line(-1);
+	printf("Res1: %s", res);
+	free(res);
+	res = get_next_line(fd);
+	printf("Res2: %s", res);
+	free(res);
+	res = get_next_line(fd);
+	printf("Res3: %s", res);
+	free(res);
+	res = get_next_line(fd);
+	printf("Res4: %s", res);
+	free(res);
+	res = get_next_line(fd);
+	printf("Res5: %s", res);
+	free(res);
+	res = get_next_line(fd);
+	printf("Res6: %s", res);
+
+	free(res);
+
+	close(fd);
+}*/
